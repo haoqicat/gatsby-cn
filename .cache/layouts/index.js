6de0131d6c8a84d44bec95c34438cb34cc15c27a
youngjingwen <798806504@qq.com>
2017-12-27 10:06:33 +0800

    first commit

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/Packing/Desktop/gatsby/www/src/layouts/index.js"
+  import data from "/Users/Packing/Desktop/gatsby/www/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
